b8b53e8865faea26822ea33726b8a5449d77ca1d
Riley Parsons <parsons.rileyj@gmail.com>
2025-08-29 16:52:55 +1200

    made lines func in dirichet

--- fd_lab/functions_fd.py
+++ fd_lab/functions_fd.py
@@ -74,6 +74,49 @@ class SolverPoissonXY(object):
         mesh points along the Dirichlet boundaries.
         """
         # TODO - your code here
+        
+        def lines(side):
+            """ Helper function returning boundary indexes for various sides
+
+            Args:
+                side (_type_): _description_
+
+            Returns:
+                _type_: _description_
+            """
+            if side == "B":
+                app = list(range(0, self.nx))
+                
+            if side == "T":
+                app = list(range(self.n-self.nx, self.n))
+                
+            if side == "L":
+                app = np.linspace(0, self.n-self.x, self.y)
+                app = app.tolist()
+                
+            if side == "R":
+                app = np.linspace(self.x-1, self.n-1, self.y)
+                app = app.tolist()
+                
+            app = [int(val) for val in app] #convert to list of ints
+            return app
+        
+        for n in range(0,self.n):
+            ind = 0
+            for y in self.y:
+                for x in self.x:
+                    A[n, ind] = 0
+                
+        
+            
+                
+  
+
+            
+
+                
+            
+        
         pass
 
     def neumann(self):
